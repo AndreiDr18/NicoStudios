8c9d110ff3ed1d95d87ffe6668bb351c4434da48
AndreiDr18 <43674342+AndreiDr18@users.noreply.github.com>
2021-05-15 17:30:04 +0300

    Added captcha

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,3 @@
-
 //File upload handler
 const multer = require('multer');
 
@@ -19,47 +18,69 @@ const cookies = require('cookies');
 const cookieKeys = ['Crest MapleDoor AveMaria NemtudomRuumano'];
 
 //Email System
-let nodemailer = require('nodemailer')
+let nodemailer = require('nodemailer');
 
-async function sendMail(){
+//Fetch
+const fetch = require('node-fetch');
+
+/*async function sendMail() {
   let transporter = nodemailer.createTransport({
-    service:'Gmail',
-    port:2565,
-    auth:{
-      user:'dragneaandrei2001@gmail.com',
-      pass:'Bet0ovY20013'
+    service: 'Gmail',
+    port: 2565,
+    auth: {
+      user: 'dragneaandrei2001@gmail.com',
+      pass: 'Bet0ovY20013'
     }
   });
 
   let info = await transporter.sendMail({
-    from:'"Dragnea Andrei" <dragneaandrei2001@gmail.com>',
-    to:'dragneaionutandrei@gmail.com',
-    subject:'Merge ba!',
-    text:'Merge si aici, stai chill'
+    from: '"Dragnea Andrei" <dragneaandrei2001@gmail.com>',
+    to: 'dragneaionutandrei@gmail.com',
+    subject: 'Merge ba!',
+    text: 'Merge si aici, stai chill'
   });
 
-  console.log('blavo cplm sa zic: '+ info.messageId);
+  console.log('blavo cplm sa zic: ' + info.messageId);
   console.log(nodemailer.getTestMessageUrl(info))
+}
+*/
+async function recaptchaCheck(token, res){
+  const response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=6LeA7NUaAAAAAPzHEZe6hfcJ8GLLejgw6bF1PZ5a&response=${token}`, {
+    method:'POST'
+  });
+  const data = await response.json();
+  return data;
+
 }
 
 function orderGET(req, res) {
-    res.render('order');
+  res.render('order');
 }
 
-function orderPOST(req, res) {
+async function orderPOST(req, res) {
 
-  let cookie = new cookies(req, res, {keys:cookieKeys});
+  let cookie = new cookies(req, res, {
+    keys: cookieKeys
+  });
   let alreadyOrdered = cookie.get('alreadyOrdered');
 
-    session.orderValidation = {
-        name:true,
-        email:true,
-        shortDescription:true,
-        details:true
-    };
-    if ((emailValidator.validate(req.body.email)) && (!(alreadyOrdered))) {
+
+  session.orderValidation = {
+    name: true,
+    email: true,
+    shortDescription: true,
+    details: true
+  };
+  //Human Validation
+
+  let humanValidate = await recaptchaCheck(req.body["g-recaptcha-response"]);
+
+  //Check for troll requests
+
+      if ((emailValidator.validate(req.body.email))/* &&  (!(alreadyOrdered)) */ && (humanValidate["success"])) {
         console.log(alreadyOrdered + '\n');
         cookie.set('alreadyOrdered', true);
+
         let order = new orderModel;
         order.name = `${req.body.name}`;
         order.email = `${req.body.email}`;
@@ -67,29 +88,31 @@ function orderPOST(req, res) {
         order.shortDescription = `${req.body.shortDescription}`;
         order.details = `${req.body.details}`;
         order.save()
-            .then(res => {
-                fs.rename(`./public/img/orderReferences/${req.body.name}.jpg`, `./public/admin/orders/view/img/orderReferences/${order._id}.jpg`, () => {
-                    console.log('File Renamed');
-                    sendMail()
-                    .catch(err =>{ console.log(err);});
+          .then(res => {
+            fs.rename(`./public/img/orderReferences/${req.body.name}.jpg`, `./public/admin/orders/view/img/orderReferences/${order._id}.jpg`, () => {
+              sendMail()
+                .catch(err => {
+                  console.log(err);
                 });
 
             });
-        res.redirect('/');
 
-    } else if (emailValidator.validate(req.body.email) == undefined) {
+          });
+          res.redirect('/');
+        //res.redirect('/');
+
+      } else if (emailValidator.validate(req.body.email) == undefined) {
         console.log('req.body.email undefined at line 13 orderController');
-        console.log(alreadyOrdered + '\n');
         res.redirect('/');
-    } else {
+      } else {
         session.orderValidation.email = false;
-        console.log("Nu merge bro")
         console.log(alreadyOrdered + '\n');
         res.redirect('/');
-    }
+      }
+
 }
 
 module.exports = {
-    orderGET,
-    orderPOST
+  orderGET,
+  orderPOST
 };
