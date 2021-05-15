0c51ac368d279dde8f74ccfce78eda4e3010ff55
AndreiDr18 <43674342+AndreiDr18@users.noreply.github.com>
2021-05-15 18:17:33 +0300

    contact form + contact captcha

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,5 +1,47 @@
+//Mongoose contact model
+const contactModel = require('../models/contact.js');
+
+//Email Validation
+const emailValidator = require('email-validator');
+
+//Fetch for backend
+const fetch = require('node-fetch');
+
+async function validateHuman(token){
+  let response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=6LcPyNMaAAAAADLxC34cyjYrks1A5tm_6T6nrgyP&response=${token}`
+  ,{
+    method:'POST'
+  });
+
+  const data = response.json();
+
+  return data;
+}
+
+
 function contactGET(req, res){
     res.render('contact');
 }
 
-module.exports = {contactGET};
+async function contactPOST(req, res){
+  const humanValidation = await validateHuman(req.body['g-recaptcha-response']);
+  const isHuman = humanValidation.success;
+
+    if((emailValidator.validate(req.body.email) && isHuman)){
+      let contactEntry = new contactModel();
+
+
+      contactEntry.name = req.body.name;
+      contactEntry.email = req.body.email;
+      contactEntry.subject = req.body.subject;
+      contactEntry.message = req.body.message;
+      contactEntry.save()
+      .catch(err=>{
+        console.log(err);
+      });
+
+    }
+    res.redirect('/');
+}
+
+module.exports = {contactGET, contactPOST};
--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/contactController');
 
 router.get('/', controller.contactGET);
+router.post('/', controller.contactPOST);
+
 
 module.exports = router;
